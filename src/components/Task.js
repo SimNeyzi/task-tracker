cbebd8fbd8ecc3b6a81792599ac409a73cff326a
Sim Neyzi <simneyzi@gmail.com>
2022-09-07 17:06:40 -0700

    add form

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,19 @@
-function Task({ task }) {
+import { FaTimes } from "react-icons/fa";
+
+function Task({ task, onDelete, onToggle }) {
   return (
-    <div className='task'>
-      <h3>{task.text}</h3>
+    <div
+      className={`task ${task.reminder ? "reminder" : ""}`}
+      onDoubleClick={() => onToggle(task.id)}
+    >
+      <h3>
+        {task.text}
+        {"   "}
+        <FaTimes onClick={() => onDelete(task.id)} />
+      </h3>
       <p>{task.day}</p>
     </div>
-  )
+  );
 }
 
-export default Task
+export default Task;
